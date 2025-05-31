8abb0a84910a00f6edf9e82d17328fb15d355916
defton290 <70007384+defton290@users.noreply.github.com>
2025-05-31 14:26:04 +0800

    Git ignore edits

---
+++ backend/app/components/test.py
@@ -0,0 +1,6 @@
+def test1():
+    """
+    This is a test function that does nothing.
+    It is used to ensure that the test framework is working correctly.
+    """
+    print("Test function executed successfully.")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,21 @@
 from typing import Union
 
 from fastapi import FastAPI
+from components import test1
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    test()
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    test1()
